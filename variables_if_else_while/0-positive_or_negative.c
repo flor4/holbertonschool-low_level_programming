d61b343f60234795248d0de5d4d91492c404126d
flor4 <salanson.flora@outlook.fr>
2025-06-17 15:03:04 +0200

    create a program with the if condition

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - projet
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+	printf("is positive \n");
+	}
+	if (n == 0)
+	{
+	printf("is zero \n");
+	}
+	if (n < 0)
+	{
+	printf("is negative \n");
+	}
+	return (0);
+}
