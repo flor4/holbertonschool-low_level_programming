727d9d38291dc93f1b27aa59d1ea1fdcf196f719
flor4 <salanson.flora@outlook.fr>
2025-06-29 23:20:52 +0200

    change: ascii to a letter, and use a while

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -12,17 +12,15 @@
 char *string_toupper(char *str)
 
 {
-	int i;
+	int i = 0;
 
-
-	for (i = 0; i < str[i] ; i++)
+	while (str[i] != '\0')
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		if (str[i] >= 97 && str[i] <= 122)
+		{
 			str[i] = str[i] - 32;
-
-		else if (str[i] >= 'A' && str[i] <= 'Z')
-			str[i] = str[i] + 32;
-
+		}
+	i++;
 	}
 	return (str);
 }
