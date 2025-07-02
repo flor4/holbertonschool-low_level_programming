f5d75751a62439bae477d0dc1d7cf50368185120
flor4 <salanson.flora@outlook.fr>
2025-07-02 13:32:39 +0200

    fix code error

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,37 +1,28 @@
 #include "main.h"
 
-
-
 /**
  * *_strstr - Write a function that locates a substring.
+ * @haystack: string to search in
+ * @needle: to search for
  *
- * @haystack: string.
- *
- * @needle: string.
- *
- * Return: char in commun.
- *
+ * Return: 0
  */
 
 char *_strstr(char *haystack, char *needle)
-
 {
-	unsigned int i, j;
+	int i, j;
 
 	for (i = 0; haystack[i] != '\0'; i++)
+	{
 		for (j = 0; needle[j] != '\0'; j++)
 		{
-
 			if (haystack[i + j] != needle[j])
 			{
 				break;
 			}
-
-			if (needle[j] != '\0')
-				return (haystack + i);
-
-
 		}
-
+		if (needle[j] == '\0')
+			return (&haystack[i]);
+	}
 	return (0);
 }
