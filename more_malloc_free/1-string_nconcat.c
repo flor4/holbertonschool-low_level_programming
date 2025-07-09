21feeda96e374c71ee54e23c921ac1df4dc7cf78
flor4 <salanson.flora@outlook.fr>
2025-07-09 11:48:37 +0200

    fix: change the structure of the code

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,25 +12,31 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, len_s1, len_s2, copy_len_s2, total_size;
-	char *new_string;
+	char *result;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	len_s1 = (s1 == NULL) ? 0 : strlen(s1);
-	len_s2 = (s2 == NULL) ? 0 : strlen(s2);
-	copy_len_s2 = (n >= len_s2) ? len_s2 : n;
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
 
-	total_size = len_s1 + copy_len_s2 + 1;
-	new_string = malloc(total_size);
-	if (new_string == NULL)
+	while (s1[len1])
+		len1++;
+	while (s2[len2])
+		len2++;
+
+	if (n > len2)
+		n = len2;
+
+	result = malloc(len1 + n + 1);
+	if (!result)
 		return (NULL);
 
-	for (i = 0; i < len_s1; i++)
-		new_string[i] = s1[i];
+	for (; i < len1; i++)
+		result[i] = s1[i];
+	for (; j < n; j++)
+		result[i + j] = s2[j];
 
-	for (j = 0; j < copy_len_s2; j++)
-		new_string[i + j] = s2[j];
-
-	new_string[i + j] = '\0';
-
-	return (new_string);
+	result[i + j] = '\0';
+	return (result);
 }
