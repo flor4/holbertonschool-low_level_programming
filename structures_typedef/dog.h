0c6251525891f86022b18bc393373165b315d952
flor4 <salanson.flora@outlook.fr>
2025-07-11 08:54:56 +0200

    create a header file with struct dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - information about.
+ *
+ * @name: name of.
+ * @age : age of.
+ * @owner: owner of.
+ *
+ *
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
