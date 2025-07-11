337a1136f7e1b7ce259001cc78b765589bcb5661
flor4 <salanson.flora@outlook.fr>
2025-07-11 15:33:26 +0200

    fix: error about float .1 .6

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -15,22 +15,27 @@ void print_dog(struct dog *d)
 
 {
 	if (d == NULL)
+	{
 		return;
-	
+	}
 	if (d->name == NULL)
-
+	{
 		printf("Name: (nil)\n");
-			else
-				printf("Name: %s\n", d->name);
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
 
-
-	printf("Age: %.1f\n", d->age);
+	printf("Age: %.6f\n", d->age);
 
 	if (d->owner == NULL)
-
+	{
 		printf("Owner: (nil)\n");
-			else
-				printf("Owner: %s\n", d->owner);
-	
+	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
 
 }
