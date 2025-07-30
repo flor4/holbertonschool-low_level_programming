6e9616465f32c1424927bb1543b138a7ffc3ca44
flor4 <salanson.flora@outlook.fr>
2025-07-30 20:47:56 +0200

    Create a function that print the number of element

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_dlistint -  a function that prints
+ * all the elements of a dlistint_t list.
+ *
+ * @h: header node.
+ *
+ * Return: Number.
+ */
+
+
+size_t print_dlistint(const dlistint_t *h)
+
+{
+	unsigned int n = 0;
+	while (h)
+	{
+		printf("%i\n", h->n);
+
+			h = h->next;
+			n++;
+	}
+
+	return (n);
+}
