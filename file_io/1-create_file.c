26d6ad014e7fd74b7393355925c44714313b9102
flor4 <salanson.flora@outlook.fr>
2025-08-03 12:48:34 +0200

    Create: function that creates a file.

---
+++ file_io/1-create_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * create_file - a function that creates a file.
+ *
+ * @filename: is the name of the file to create.
+ * @text_content: is a NULL terminated string to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ *
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	size_t len;
+	ssize_t n_written;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (file == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		len = strlen(text_content);
+		n_written = write(file, text_content, len);
+
+		if (n_written == -1 || n_written != (ssize_t)len)
+		{
+			close(file);
+			return (-1);
+		}
+	}
+	if (close(file) == -1)
+		return (-1);
+	return (1);
+}
