56a84a0a8feba20cb3487778a08a038780ed53e7
flor4 <salanson.flora@outlook.fr>
2025-08-03 15:36:37 +0200

    fix: cat hello file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (file == -1)
 		return (-1);
 
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
 		while (text_content[len])
 			len++;
@@ -36,6 +36,6 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	close(file);
-	return (-1);
+	return (1);
 }
 
