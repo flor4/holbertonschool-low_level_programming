900875d51e723e5969c2b27fbbd5c97ef55f36af
flor4 <salanson.flora@outlook.fr>
2025-08-03 20:14:50 +0200

    Create: a function that adds an element to the hash table.

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,56 @@
+#include "hash_tables.h"
+
+/**
+ *  hash_table_set -  a function that adds an element to the hash table.
+ *
+ * @ht: is the hash table you want to add or update the key/value to.
+ * @key: key.
+ * @value: is the value associated with the key.
+ *
+ * Return: 1 if it succeeded, 0 otherwise.
+ *
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node, *current;
+	char *value_copy;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	current = ht->array[index];
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			value_copy = strdup(value);
+			if (value_copy == NULL)
+				return (0);
+			current->value = value_copy;
+			return (1);
+		}
+		current = current->next;
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	if (new_node->key == NULL || new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node->value);
+		free(new_node);
+		return (0);
+	}
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
+
+}
